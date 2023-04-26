02c2e135a20f49962a4bdfe9be53e79d4e87348b
vdeng9 <50648395+vdeng9@users.noreply.github.com>
2023-04-26 16:16:34 -0400

    Add files via upload

--- Discordbot/basic.py
+++ Discordbot/basic.py
@@ -1,7 +1,7 @@
 import discord
 from discord.ext import commands
 import random
-import re, os, sys
+import re, os, sys, requests
 
 vtubers = [["Omaru Polka", "https://www.youtube.com/@OmaruPolka"], ["Ceres Fauna", "https://www.youtube.com/@CeresFauna"], ["Nanashi Mumei", "https://www.youtube.com/@NanashiMumei"], ["Usada Pekora", "https://www.youtube.com/@UsadaPekora"], ["Sakamata Chloe", "https://www.youtube.com/@SakamataChloe"],
            ["Laplus Darknesss", "https://www.youtube.com/@LaplusDarknesss"], ["Hakui Koyori", "https://www.youtube.com/@HakuiKoyori"], ["Yukihana Lamy", "https://www.youtube.com/@YukihanaLamy"], ["Momosuzu Nene", "https://www.youtube.com/@MomosuzuNene"], ["Shishiro Botan", "https://www.youtube.com/@ShishiroBotan"],
@@ -108,7 +108,7 @@ class basicplugin(commands.Cog):
         '''Echos input'''
         output = ''
         for message in args:
-            output += message
+            output += message + " "
         await ctx.send(output)
 
     @commands.command(name="goodbot")
@@ -122,3 +122,30 @@ class basicplugin(commands.Cog):
         '''Insult the bot :^('''
         await ctx.send("Faq you peko! {}".format(ctx.message.author.mention))
         await ctx.send("https://tenor.com/view/hololive-vtuber-usada-pekora-crazy-usagi-laser-gif-16904860")
+
+    @commands.command(name="weather", aliases=["w"])
+    async def weatherapi(self, ctx, lat, long):
+        '''Get weather using lat and long (no more than 4 decimal places idk y it dont work and i dont wanna fix it)'''
+        weatherurl = "https://api.weather.gov/points/" + lat + ',' + long
+        #await ctx.send(weatherurl) #check url is created correctly (works)
+        reponse = requests.get(weatherurl)
+        reponseJSON = reponse.json()
+        forecast_url = reponseJSON['properties']['forecast']
+        relativelocation = reponseJSON['properties']['relativeLocation']['properties']
+        city = relativelocation['city']
+        state = relativelocation['state']
+        #await ctx.send(forecast_url)
+        forecast_res = requests.get(forecast_url)
+        forecastJSON = forecast_res.json()
+        forecast_data = []
+        row, col = 10,2
+        for i in range(row):
+            column = []
+            for j in range(col):
+                if j == 0:
+                    column.append(forecastJSON['properties']['periods'][i]['name'])
+                else:
+                    column.append(forecastJSON['properties']['periods'][i]['detailedForecast'])
+            forecast_data.append(column)
+        await ctx.send(city + ", " + state)
+        await ctx.send(str(forecast_data))
